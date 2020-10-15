e122b839873457d9d88334c1dc8a617007ceb060
juhou_hallym1 <trikbee@gmail.com>
2020-10-15 19:24:20 +0900

    debug contour removal

--- unet_pipeline/experiments/albunet1024/albunet1024.sh
+++
@@ -1,6 +0,0 @@
-cd unet_pipeline
-python Train.py ./experiments/albunet1024/01_train_config_part0.yaml
-python Train.py ./experiments/albunet1024/02_train_config_part1.yaml
-python Inference.py ./experiments/albunet1024/04_inference_config.yaml
-python TripletSubmit.py ./experiments/albunet1024/05_submit.yaml
-
